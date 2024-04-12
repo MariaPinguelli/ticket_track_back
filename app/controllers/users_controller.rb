3673d4a0d28e27b176f524237f75c62fd5f1ed8c
MariaPinguelli <mariaferpinguelli@hotmail.com>
2024-04-12 19:40:31 -0300

    start user config

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @users = User.find(params[:id])
+    end
+
+    def create
+        @users = User.new(user_params)
+        @users.save
+    end
+
+    def update
+        @users = User.find(params[:id])
+    end
+
+    def destroy
+        @users = User.find(params[:id])
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+end
